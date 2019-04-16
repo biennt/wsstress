bf96472acbc8a765bbf00cc621c185333f3b06c2
Nguyen Thanh Bien <bien@bienlab.com>
2019-04-16 10:35:50 +0700

    Update ws-client.js

--- ws-client.js
+++ ws-client.js
@@ -8,368 +8,88 @@ client.on('connectFailed', function(error) {
 });
 
 client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
+    console.log("'WebSocket Client Connected'"+Date(Date.now()).toString()+"'");
+
     connection.on('error', function(error) {
         console.log("Connection Error: " + error.toString());
     });
     connection.on('close', function() {
-        console.log('Connection Closed');
+        console.log("'Connection Closed'" + Date(Date.now()).toString()+"'");
     });
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
+//            console.log("Received: '" + message.utf8Data + "'");
         }
     });
-    
+
     function sendInit() {
-	var msginit = {
-		type: "connection_init",
-		payload: {}
-	};
+        var msginit = {
+                type: "connection_init",
+                payload: {}
+        };
 var msg1 = {
-	"id": "1",
-	"type": "start",
-	"payload": {
-		"variables": {},
-		"extensions": {},
-		"operationName": null,
-		"query": "subscription {\n  systemStatusChanged {\n    status\n    __typename\n  }\n}\n"
-	}
+        "id":"1",
+ "type":"start",
+ "payload":{
+	         "variables":{},
+             "extensions":{},"operationName":null,
+             "query":"subscription {\n  systemStatusChanged {\n    status\n    __typename\n  }\n}\n"
+		   }
 };
 
 var msg2 = {
-	"id": "2",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VN30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
+"id":"2",
+  "type":"start",
+  "payload":{
+              "variables":{"markets":["hose","hnx","upcom"]},
+			  "extensions":{},"operationName":"notifySessionByList",
+              "query":"subscription notifySessionByList($markets: [String!]!) {\n  notifySessionByList(markets: $markets) {\n    market\n    session\n    __typename\n  }\n}\n"
+			}
 };
 
 
 var msg3 = {
-	"id": "3",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VN30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexChart",
-		"query": "subscription notifyIndexChart($indexId: String!) {\n  notifyIndexChart(indexId: $indexId) {\n    indexID\n    data\n    __typename\n  }\n}\n"
-	}
+        "id":"3",
+ "type":"start",
+ "payload":{
+             "variables":{"indexIds":["VNINDEX","VN30","HNX30","HNXIndex","HNXUpcomIndex","VNXALL"]},
+			 "extensions":{},"operationName":"notifyIndexRealtimeByList",
+			 "query":"subscription notifyIndexRealtimeByList($indexIds: [String!]!) {\n  notifyIndexRealtimeByList(indexIds: $indexIds) {\n    indexID\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
+		   }
 };
 
 var msg4 = {
-	"id": "4",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VN30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    indexID\n    totalQtty\n    allQty\n    allValue\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
+"id":"4",
+ "type":"start",
+ "payload":{
+             "variables":{"arr":["hose:572","hose:582","hose:580","hose:629","hose:634","hose:792","hose:974","hose:1152","hose:1146","hose:1386","hose:1354","hose:2027","hose:2024","hose:2028","hose:2200","hose:2557","hose:2735","hose:2740","hose:2947","hose:2923","hose:2920","hose:2908","hose:32","hose:3825","hose:29","hose:3813","hose:3847","hose:3801","hose:3852","hose:3854"]},
+			 "extensions":{},
+			 "operationName":"stockRealtimeByList",
+			 "query":"subscription stockRealtimeByList($arr: [String]!) {\n  stockRealtimeByList(arr: $arr) {\n    data\n    __typename\n  }\n}\n"
+		   }
 };
 
 var msg5 = {
-	"id": "5",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"market": "hose"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($market: String!) {\n  notifySession(market: $market) {\n    session\n    __typename\n  }\n}\n"
-	}
+	"id":"5",
+	"type":"start",
+	"payload":{"variables":{"indexIds":["VNINDEX","VN30","HNX30","HNXIndex","HNXUpcomIndex","VNXALL"]},"extensions":{},"operationName":"notifyIndexRealtimeByList",
+	"query":"subscription notifyIndexRealtimeByList($indexIds: [String!]!) {\n  notifyIndexRealtimeByList(indexIds: $indexIds) {\n    indexID\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
+}
 };
 
-var msg6 = {
-	"id": "6",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "HNX30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexChart",
-		"query": "subscription notifyIndexChart($indexId: String!) {\n  notifyIndexChart(indexId: $indexId) {\n    indexID\n    data\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg7 = {
-	"id": "7",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "HNX30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    indexID\n    totalQtty\n    allQty\n    allValue\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-var msg8 = {
-	"id": "8",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"market": "hose"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($market: String!) {\n  notifySession(market: $market) {\n    session\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg9 = {
-	"id": "9",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VN30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexChart",
-		"query": "subscription notifyIndexChart($indexId: String!) {\n  notifyIndexChart(indexId: $indexId) {\n    indexID\n    data\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg10 = {
-	"id": "10",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VN30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    indexID\n    totalQtty\n    allQty\n    allValue\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg11 = {
-	"id": "11",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"market": "hose"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($market: String!) {\n  notifySession(market: $market) {\n    session\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg12 = {
-	"id": "12",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "HNXIndex"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexChart",
-		"query": "subscription notifyIndexChart($indexId: String!) {\n  notifyIndexChart(indexId: $indexId) {\n    indexID\n    data\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg13 = {
-	"id": "13",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "HNXIndex"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    indexID\n    totalQtty\n    allQty\n    allValue\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg14 = {
-	"id": "14",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"market": "hnx"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($market: String!) {\n  notifySession(market: $market) {\n    session\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg15 = {
-	"id": "15",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VNINDEX"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg16 = {
-	"id": "16",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "HNXUpcomIndex"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg17 = {
-	"id": "17",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "HNXIndex"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg18 = {
-	"id": "18",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "HNX30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg19 = {
-	"id": "19",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VNXALL"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    allQty\n    allValue\n    totalQtty\n    totalValue\n    advances\n    declines\n    nochanges\n    ceiling\n    floor\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg810 = {
-	"id": "810",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"stockNo": "fu:1097"
-		},
-		"extensions": {},
-		"operationName": "stockRealtimeChanged",
-		"query": "subscription stockRealtimeChanged($stockNo: String!) {\n  stockRealtimeChanged(stockNo: $stockNo) {\n    data\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg811 = {
-	"id": "811",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"stockNo": "fu:1104"
-		},
-		"extensions": {},
-		"operationName": "stockRealtimeChanged",
-		"query": "subscription stockRealtimeChanged($stockNo: String!) {\n  stockRealtimeChanged(stockNo: $stockNo) {\n    data\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg812 = {
-	"id": "812",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"stockNo": "fu:1100"
-		},
-		"extensions": {},
-		"operationName": "stockRealtimeChanged",
-		"query": "subscription stockRealtimeChanged($stockNo: String!) {\n  stockRealtimeChanged(stockNo: $stockNo) {\n    data\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg813 = {
-	"id": "813",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"stockNo": "fu:1103"
-		},
-		"extensions": {},
-		"operationName": "stockRealtimeChanged",
-		"query": "subscription stockRealtimeChanged($stockNo: String!) {\n  stockRealtimeChanged(stockNo: $stockNo) {\n    data\n    __typename\n  }\n}\n"
-	}
-};
-
-var msg814 = {
-	"id": "814",
-	"type": "start",
-	"payload": {
-		"variables": {
-			"indexId": "VN30"
-		},
-		"extensions": {},
-		"operationName": "notifyIndexRealtime",
-		"query": "subscription notifyIndexRealtime($indexId: String!) {\n  notifyIndexRealtime(indexId: $indexId) {\n    indexValue\n    __typename\n  }\n}\n"
-	}
-};
 
 
         if (connection.connected) {
-			console.log('Sending init' + JSON.stringify(msginit));
-			connection.send(JSON.stringify(msginit));
-			connection.send(JSON.stringify(msg1));
-			connection.send(JSON.stringify(msg2));			
-			connection.send(JSON.stringify(msg3));			
-			connection.send(JSON.stringify(msg4));			
-			connection.send(JSON.stringify(msg5));			
-			connection.send(JSON.stringify(msg6));	
-			connection.send(JSON.stringify(msg7));
-			connection.send(JSON.stringify(msg8));
-			connection.send(JSON.stringify(msg9));
-			connection.send(JSON.stringify(msg10));
-			connection.send(JSON.stringify(msg11));			
-			connection.send(JSON.stringify(msg12));
-			connection.send(JSON.stringify(msg13));
-			connection.send(JSON.stringify(msg14));
-			connection.send(JSON.stringify(msg15));
-			connection.send(JSON.stringify(msg16));			
-			connection.send(JSON.stringify(msg17));			
-			connection.send(JSON.stringify(msg18));			
-			connection.send(JSON.stringify(msg19));						
-			connection.send(JSON.stringify(msg810));									
-			connection.send(JSON.stringify(msg811));									
-			connection.send(JSON.stringify(msg812));									
-			connection.send(JSON.stringify(msg813));									
-			connection.send(JSON.stringify(msg814));									
-        }
+                        console.log('Sending init' + JSON.stringify(msginit));
+                        connection.send(JSON.stringify(msginit));
+                        connection.send(JSON.stringify(msg1));
+                        connection.send(JSON.stringify(msg2));
+                        connection.send(JSON.stringify(msg3));
+                        connection.send(JSON.stringify(msg4));
+                        connection.send(JSON.stringify(msg5));
+         }
     }
     sendInit();
 });
 
 client.connect('ws://realtime.kompa.vn/graphql','graphql-ws');
-
-  
-
